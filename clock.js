223cf574b938621279a6bf6a62b70226741fe317
aziz <erazizeee@gmail.com>
2022-08-09 22:15:20 +0530

    analog clock

---
+++ clock.js
@@ -0,0 +1,23 @@
+let secDiv = document.getElementById("second");
+// console.log(sec);
+let minDiv = document.getElementById("minute");
+// console.log(min);
+let hourDiv = document.getElementById("hour");
+// console.log(hour);
+
+setInterval(update, 1000);
+
+function update() {
+    let date = new Date();
+    let sec = date.getSeconds() / 60;
+    let min = date.getMinutes() / 60;
+    let hour = date.getHours() / 60;
+
+    secDiv.style.transform = "rotate(" + (sec * 360)+"deg)";
+    // console.log(secDiv)
+    minDiv.style.transform = "rotate(" + (min * 360) +"deg)";
+    // console.log(minDiv);
+    hourDiv.style.transform = "rotate("+(hour* 360)+"deg)";
+    // console.log(hourDiv);
+}
+update();
