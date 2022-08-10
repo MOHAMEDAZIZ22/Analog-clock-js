9debcc5c09948a01fa98024da2f2dd9d4beef9e0
aziz <erazizeee@gmail.com>
2022-08-10 10:12:43 +0530

    Analog clock

--- clock.js
+++ clock.js
@@ -11,7 +11,7 @@ function update() {
     let date = new Date();
     let sec = date.getSeconds() / 60;
     let min = date.getMinutes() / 60;
-    let hour = date.getHours() / 60;
+    let hour = date.getHours() / 12;
 
     secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
     // console.log(secDiv)
